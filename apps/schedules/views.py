df233c845b689abcf78836865237c29d0f156024
aeud <adrien.eudes@gmail.com>
2015-12-01 11:05:19 +0800

    Added schedules to everyone + lis

--- apps/schedules/views.py
+++ apps/schedules/views.py
@@ -3,7 +3,9 @@ from apps.schedules.models import Schedule, ScheduleOption
 
 def index(request):
     schedules = Schedule.objects.filter(created_by=request.user, is_active=True)
-    return render(request, 'schedules/index.html', dict(schedules=schedules))
+    concerned_schedules = Schedule.objects.filter(email=request.user.email, is_active=True).exclude(id__in=[s.id for s in schedules])
+    return render(request, 'schedules/index.html', dict(schedules=schedules,
+                                                        concerned_schedules=concerned_schedules,))
 
 def remove(request, schedule_id):
     schedule = get_object_or_404(Schedule, created_by=request.user, is_active=True, pk=schedule_id)
