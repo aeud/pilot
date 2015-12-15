64ef22d30d5eabf09191be51f80aaf19fea30549
aeud <adrien.eudes@gmail.com>
2015-12-15 09:00:22 +0800

    Added Schedule Dashboard

--- apps/schedules/models.py
+++ apps/schedules/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from apps.visualizations.models import Visualization
+from apps.dashboards.models import Dashboard
 from apps.accounts.models import User
 
 class Schedule(models.Model):
@@ -20,3 +21,26 @@ class Schedule(models.Model):
 class ScheduleOption(models.Model):
     schedule  = models.ForeignKey(Schedule)
     option    = models.IntegerField()
+
+class DashboardSchedule(models.Model):
+    dashboard      = models.ForeignKey(Dashboard)
+    created_at     = models.DateTimeField(auto_now_add=True)
+    created_by     = models.ForeignKey(User)
+    email          = models.CharField(max_length=255)
+    time           = models.CharField(max_length=255)
+    frequency      = models.CharField(max_length=255)
+    is_active      = models.BooleanField(default=True)
+    anonymous_link = models.BooleanField(default=False)
+
+    def generate_subject(self):
+        return 'Colors: Your report ' + self.dashboard.name + ' is ready. [' + timezone.now().strftime('%d/%m/%Y') + ', ' + self.time + ']'
+
+class DashboardScheduleOption(models.Model):
+    schedule  = models.ForeignKey(DashboardSchedule)
+    option    = models.IntegerField()
+
+class DashboardScheduleVisualization(models.Model):
+    visualization  = models.ForeignKey(Visualization)
+    position       = models.IntegerField()
+    is_image       = models.BooleanField(default=False)
+    show_sum       = models.BooleanField(default=False)
